8f8ab072690c5ea6c6e99ee3e0de4eff241729ea
Nicholas Sully <nicholas.sully@digital.justice.gov.uk>
2020-10-22 14:27:17 +0100

    adding mocha and chia with a failing test

---
+++ test/test.js
@@ -0,0 +1,21 @@
+const assert = require('chai').assert
+// const assert = chai.assert;
+
+// const calculator = require('../src/')
+
+const employees = [
+    { name: 'Max', age: 17 },
+    { name: 'Sepp', age: 18 },
+    { name: 'Nina', age: 15 },
+    { name: 'Mike', age: 51 },
+  ];
+
+describe('employeeReport', function() {
+  describe('with a list of employees', function() {
+    it('should return everyone over 18', function() {
+        let result = employeeReport.getAllOver18(employees);
+
+        assert.equal(['Sepp', 'Mike'], result);
+    });
+  });
+});
